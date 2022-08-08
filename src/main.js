12985f9a491711574e950b103523a219301c46e9
Malo Grall <malo.grall@gmail.com>
2022-08-08 11:46:07 +0200

    Accessing files only in Note folder

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // const notionApiKey = PropertiesService.getScriptProperties().getProperty('NOTION_API_KEY')
 
+const noteFolderId = '1ic5zK6EdXOtbuvw-JDsfg2gnOyB7Uy5_'
+
 function listFiles () {
-  const files = DriveApp.getFilesByType('application/pdf')
+  const files = DriveApp.getFolderById(noteFolderId).getFilesByType('application/pdf')
 
   const file = files.next()
-  // file.setSharing(DriveApp.Access.PRIVATE, DriveApp.Permission.EDIT)
+  file.setSharing(DriveApp.Access.ANYONE_WITH_LINK, DriveApp.Permission.VIEW)
 
   const fileId = file.getId()
   const displayUrl = 'https://drive.google.com/uc?export=view&id=' + fileId
