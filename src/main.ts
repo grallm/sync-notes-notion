68e329a72f0b7f3eb18eebca01b1ef3dfe6d8fb1
Malo Grall <malo.grall@gmail.com>
2022-08-12 18:54:51 +0200

    Added clasp ignore + ESLint TS + Fetching page title and ID

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { NotionDB } from './types'
+import * as NotionApiEndpoints from '@notionhq/client/build/src/api-endpoints'
 
 const notionApiKey = PropertiesService.getScriptProperties().getProperty('NOTION_API_KEY')
 
@@ -8,21 +8,61 @@ const noteFolderId = '1ic5zK6EdXOtbuvw-JDsfg2gnOyB7Uy5_'
 // Notion page containing all note pages
 const notionPageId = 'a1e8d191-a6af-46da-8d3d-c055cfa269aa'
 
+const notionHttpHeaders = {
+  Accept: 'application/json',
+  'Notion-Version': '2022-06-28',
+  Authorization: `Bearer ${notionApiKey}`
+}
+
+/**
+ * Retrieve the title and generated ID (not UUID) of a Notion page
+ */
+function getNotionPage (pageId: string) {
+  const getPageOpt: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+    method: 'get',
+    headers: notionHttpHeaders,
+    muteHttpExceptions: true
+  }
+
+  // Retrieve the page to access ID property value
+  const notionPageRes = UrlFetchApp.fetch(`https://api.notion.com/v1/pages/${pageId}`, getPageOpt)
+  const pageRes = JSON.parse(notionPageRes.getContentText()) as NotionApiEndpoints.PageObjectResponse
+  const idPropId = pageRes.properties.ID.id
+
+  const notionPageTitleRes = UrlFetchApp.fetch(`https://api.notion.com/v1/pages/${pageId}/properties/title`, getPageOpt)
+  const titleRes = JSON.parse(notionPageTitleRes.getContentText()) as NotionApiEndpoints.PropertyItemPropertyItemListResponse
+
+  const notionPageIdRes = UrlFetchApp.fetch(`https://api.notion.com/v1/pages/${pageId}/properties/${idPropId}`, getPageOpt)
+  const idRes = JSON.parse(notionPageIdRes.getContentText()) as NotionApiEndpoints.FormulaPropertyItemObjectResponse
+
+  Logger.log(titleRes)
+  Logger.log(idRes)
+
+  return {
+    uuid: pageId,
+    title: (titleRes.results[0] as NotionApiEndpoints.TitlePropertyItemObjectResponse).title.plain_text,
+    id: (idRes.formula as { type: string; string: string }).string
+  }
+}
+
 // eslint-disable-next-line no-unused-vars
 function listFiles () {
   // All notion pages
-  const getPageOptions: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+  const queryDbOptions: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
     method: 'post',
-    headers: {
-      Accept: 'application/json',
-      'Notion-Version': '2022-06-28',
-      Authorization: `Bearer ${notionApiKey}`
-    },
+    headers: notionHttpHeaders,
     muteHttpExceptions: true,
     payload: JSON.stringify({})
   }
-  const notionPagesDbRes = UrlFetchApp.fetch(`https://api.notion.com/v1/databases/${notionPageId}/query`, getPageOptions)
-  const notionPagesDb = JSON.parse(notionPagesDbRes.getContentText()) as NotionDB
+
+  const notionPagesDbRes = UrlFetchApp.fetch(`https://api.notion.com/v1/databases/${notionPageId}/query`, queryDbOptions)
+  const notionPagesDb = JSON.parse(notionPagesDbRes.getContentText()) as NotionApiEndpoints.QueryDatabaseResponse
+
+  if (notionPagesDb.results.length === 0) {
+    return
+  }
+
+  Logger.log(getNotionPage(notionPagesDb.results[0].id))
 
   // Drive files
   const files = DriveApp.getFolderById(noteFolderId).getFilesByType('application/pdf')
